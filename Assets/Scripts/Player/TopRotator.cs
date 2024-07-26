f72eb424b110129d9bea640d241ed5410fa50d22
Vissa <46628714+Visssarion@users.noreply.github.com>
2024-07-26 18:53:52 +0300

    Death Animation Added

--- Assets/Scripts/Player/TopRotator.cs
+++ Assets/Scripts/Player/TopRotator.cs
@@ -7,7 +7,11 @@ public class TopRotator : MonoBehaviour
     [SerializeField]
     private PlayerCharacteristics _playerCharacteristics;
     public void UpdateAngle(float angle){
-        
+        if(!enabled){return;}
         transform.eulerAngles = new Vector3(0.0f, 0.0f, angle * _playerCharacteristics.upperBodyHalfSensitivity);
+    }
+    void OnDisable()
+    {
+        transform.eulerAngles = Vector3.zero;
     }
 }
