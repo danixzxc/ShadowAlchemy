0816166fa10daabbeaa711c4afe5facbfcfddd37
danixzxc <danilagrisel@gmail.com>
2024-07-26 20:17:20 +0300

    Dash skill created

--- Assets/Scripts/Skills/DashSkill.cs
+++ Assets/Scripts/Skills/DashSkill.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class DashSkill :  Skill
 {
+
+    [SerializeField]
+    private float _velocity;
+    [SerializeField]
+    private float _distance;
+    [SerializeField]
+    private float _finalVelocity;
+
+    private Rigidbody2D _rigidbody;
+    private float _time;
     public DashSkill()
     {
         //data = CombinationManager.Instance.
     }
     public override void castSkill(float direction, GameObject player)
     {
-        Debug.Log("dash casted");
+        _time = _distance/_velocity;
+        Rigidbody2D rigidbody = player.GetComponent<Rigidbody2D>();
+        WaitForSkillEnd(direction);
     }
+
+    private IEnumerator WaitForSkillEnd(float direction)
+    {
+        float time = 0;
+        while (time < _time)
+        {
+            _rigidbody.velocity = AngleToVec2(direction) * _velocity;
+            yield return new WaitForFixedUpdate();
+            time += Time.fixedDeltaTime;
+        }
+        _rigidbody.velocity = _rigidbody.velocity / _finalVelocity;
+    }
+
 }
--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public abstract class Skill 
 {
-    public SkillData data; 
-
+    public SkillData data;
+    protected Vector2 AngleToVec2(float angle)
+    {
+        return new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+    }
     public abstract void castSkill(float direction, GameObject player);
 }
