88db19fd6859b185aafa57c034429543d3015970
danixzxc <danilagrisel@gmail.com>
2024-07-30 01:39:28 +0300

    One and two ways moving platforms prefabs

---
+++ Assets/Scripts/Entities/Platforms/OneWayMovingPlatform.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OneWayMovingPlatform : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+    private Rigidbody2D _rigidbody;
+    private Vector2 _targetPosition;
+    [SerializeField]
+    private float _time;
+
+    private float _fullDistanceX;
+    private float _fullDistanceY;
+    public bool moving { get; set; } = false;
+
+    
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _lineRenderer = GetComponent<LineRenderer>();
+
+        _targetPosition = _lineRenderer.GetPosition(0);
+        _fullDistanceX = _targetPosition.x - transform.position.x;
+        _fullDistanceY = _targetPosition.y - transform.position.y;
+
+    }
+    void FixedUpdate()
+    {
+        if (moving)
+        {
+            _rigidbody.velocity = new Vector2(_fullDistanceX / _time, _fullDistanceY / _time);
+            if (Vector2.Distance(transform.position, _targetPosition) < 1.5f)
+                _rigidbody.velocity = new Vector2(0, 0);
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        var rigidbody = collision.collider.GetComponent<Rigidbody2D>();
+        rigidbody.position = (rigidbody.position + _rigidbody.velocity * Time.fixedDeltaTime);
+        Debug.Log(_rigidbody.velocity * Time.fixedDeltaTime + " - giving to a player per second");
+    }
+
+}
---
+++ Assets/Scripts/Entities/Platforms/TwoWaysMovingPlatform.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoWaysMovingPlatform : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+    private Rigidbody2D _rigidbody;
+    private Vector2 _leftTargetPosition;
+    private Vector2 _rightTargetPosition;
+    [SerializeField]
+    private float _timeFromStartToFirstPoint;
+    [SerializeField]
+    private float _timeFromOnePointToAnother;
+    [SerializeField]
+    private bool _startFromLeftPoint;
+
+    private Vector2 _currentTargetPosition;
+
+    private float _time;
+    private Vector2 _distancePerSecond;
+
+    public bool moving { get; set; } = false;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _lineRenderer = GetComponent<LineRenderer>();
+
+        if (_lineRenderer.GetPosition(0).x < _lineRenderer.GetPosition(1).x)
+        {
+            _leftTargetPosition = _lineRenderer.GetPosition(0);
+            _rightTargetPosition = _lineRenderer.GetPosition(1);
+        }
+        else
+        {
+            _leftTargetPosition = _lineRenderer.GetPosition(1);
+            _rightTargetPosition = _lineRenderer.GetPosition(0);
+        }
+        if(_startFromLeftPoint)
+        {
+            _currentTargetPosition = _leftTargetPosition;
+        }
+        else
+        {
+            _currentTargetPosition = _rightTargetPosition;
+        }
+        _time = _timeFromStartToFirstPoint;
+        _distancePerSecond = (_currentTargetPosition - (Vector2)transform.position) / _time;
+    }
+    void FixedUpdate()
+    {
+        if (moving)
+        {
+            _rigidbody.velocity = new Vector2(_distancePerSecond.x, _distancePerSecond.y);
+            if (Vector2.Distance(transform.position, _currentTargetPosition) < 1.5f
+                && _currentTargetPosition == _leftTargetPosition)
+            {
+                _currentTargetPosition = _rightTargetPosition;
+                Reset();
+            }
+            if (Vector2.Distance(transform.position, _currentTargetPosition) < 1.5f
+                && _currentTargetPosition == _rightTargetPosition)
+            {
+                _currentTargetPosition = _leftTargetPosition;
+                Reset();
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        _time = _timeFromOnePointToAnother;
+        _distancePerSecond = (_currentTargetPosition - (Vector2)transform.position) / _time;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        var rigidbody = collision.collider.GetComponent<Rigidbody2D>();
+        rigidbody.position = (rigidbody.position + _rigidbody.velocity * Time.fixedDeltaTime);
+        Debug.Log(rigidbody.velocity);
+    }
+
+}
