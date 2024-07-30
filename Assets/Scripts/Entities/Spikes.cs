ca39894543054a576f802b95e5729bf559714b41
Vissa <46628714+Visssarion@users.noreply.github.com>
2024-07-30 12:50:55 +0300

    Added Spikes

---
+++ Assets/Scripts/Entities/Spikes.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    public bool stateOnStart = false;
+    public SpriteRenderer spriteRenderer;
+    public Collider2D trigger;
+
+    public Sprite spriteOn;
+    public Sprite spriteOff;
+    
+
+    private bool currentState = false;
+
+    void Start(){
+        Turn(stateOnStart);
+    }
+
+    public void Turn(bool state){
+        currentState = state;
+        trigger.enabled = state;
+        if(state){
+            spriteRenderer.sprite = spriteOn;
+        }
+        else{
+            spriteRenderer.sprite = spriteOff;
+        }
+    }
+
+    public void TurnOn(){
+        Turn(true);
+    }
+
+    public void TurnOff(){
+        Turn(false);
+    }
+
+    public void Switch(){
+        Turn(!currentState);
+    }
+
+}
