24d8bea2603e339c5024aa9f286a6c88c17f2e28
Vissa <46628714+Visssarion@users.noreply.github.com>
2024-07-30 13:04:45 +0300

    Added option to turn off turret

--- Assets/Scripts/Entities/Turret.cs
+++ Assets/Scripts/Entities/Turret.cs
@@ -4,21 +4,58 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    public GameObject prefab;
+    private bool _enabled = true;
+    public new bool enabled{
+        get {return _enabled;}
+        set {
+            _enabled = value;
+            if (_enabled){
+                spriteRenderer.sprite = spriteOn;
+            }
+            else{
+                time = 0.0f;
+                spriteRenderer.sprite = spriteOff;
+            }
+        }
+    }
+
+
+    public float cooldown;  
     public Transform spawningPoint;
-
-    public float cooldown;
-
+    private float time;
     public float angle = -180;
 
-    private float time;
+    public GameObject prefab;
+    public SpriteRenderer spriteRenderer;
+    public Sprite spriteOn;
+    public Sprite spriteOff;
+    
 
     void Update(){
+        if (!enabled) {
+            return;
+        }
         time += Time.deltaTime;
         if (time >= cooldown){
             time -= cooldown;
             GameObject fireball = Instantiate(prefab, spawningPoint);
             fireball.GetComponent<Fireball>().SetAngle(angle);
         }
+    }
+
+    public void Turn(bool state){
+        enabled = state;
+    }
+
+    public void TurnOn(){
+        Turn(true);
+    }
+
+    public void TurnOff(){
+        Turn(false);
+    }
+
+    public void Switch(){
+        Turn(!enabled);
     }
 }
